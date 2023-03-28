72152922e704dc03564c82107afa28c7dc18b474
Depressionsadness <129234048+Depressionsadness@users.noreply.github.com>
2023-03-29 10:59:43 +1100

    Mrbeast Takeover

---
+++ content.js
@@ -0,0 +1,3 @@
+var images = document.getElementsByTagName('img', 'iframe', 'button');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://media.tenor.com/IIXy6CqR5l8AAAAC/mrbeast-ytpmv.gif'}
